34998d25a13c7ccf26f0d6f41e311a5cab43f227
Kohei-Oyama <fkgenhf108@ezweb.ne.jp>
2017-03-06 03:12:05 +0900

    rename code

--- Client_test/ViewController.swift
+++ Client_test/ViewController.swift
@@ -11,25 +11,25 @@ import SwiftSocket
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var SendObject: UITextField!
-    @IBOutlet weak var Send_Text: UITextView!
-    @IBOutlet weak var Recieve_Text: UITextView!
+    @IBOutlet weak var sendText: UITextField!
+    @IBOutlet weak var sendTextView: UITextView!
+    @IBOutlet weak var recieveTextView: UITextView!
     
-    let host: String = "localhost"
+    let host = "localhost"
     let port: Int32 = 8084
     var client: TCPClient!
     
-    @IBAction func BT_Connect(_ sender: Any) {
+    @IBAction func connectToServer(_ sender: Any) {
         switch client.connect(timeout: 1) {
         case .success:
             print("connect!")
-            // 受信処理を別スレッドに任せる
+            // 受信待機を別スレッドに任せる
             DispatchQueue.global(qos: .background).async {
                 while true {
-                    guard let RecieveObject = self.ReceiveData() else { return }
+                    guard let recieveText = self.receiveData() else { return }
                     // UI部分の更新はメインスレッドでないといけない
                     DispatchQueue.main.async {
-                        self.appendToTextField(string: RecieveObject, view: self.Recieve_Text)
+                        self.appendToTextField(string: recieveText, view: self.recieveTextView)
                     }
                 }
             }
@@ -38,17 +38,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    @IBAction func BT_Send(_ sender: Any) {
-        //box内のMessageをsend
-        switch client.send(string: SendObject.text! ) {
+    @IBAction func sendText(_ sender: Any) {
+        //textField内のtextをsend
+        switch client.send(string: sendText.text! ) {
         case .success:
-            appendToTextField(string: SendObject.text, view: Send_Text)
+            appendToTextField(string: sendText.text, view: sendTextView)
         case .failure(let error):
             print(error)
         }
     }
     
-    @IBAction func BT_End(_ sender: Any) {
+    @IBAction func endConnection(_ sender: Any) {
         client.close()
         print("End connect")
     }
@@ -60,12 +60,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
         client = TCPClient(address: host, port: port)
         
         // textFieldの通知先とプレースホルダ
-        SendObject.delegate = self
-        SendObject.placeholder = "送りたいメッセージを入力してください"
+        sendText.delegate = self
+        sendText.placeholder = "送りたいメッセージを入力してください"
         
         // textViewを編集不可
-        Send_Text.isEditable = false
-        Recieve_Text.isEditable = false
+        sendTextView.isEditable = false
+        recieveTextView.isEditable = false
     }
     
     override func didReceiveMemoryWarning() {
@@ -79,7 +79,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    func ReceiveData() -> String! {
+    func receiveData() -> String! {
         // 何か受信したらstring返す
         guard let data = client.read(1024*10) else { return nil }
         let response = String(bytes: data, encoding: .utf8)
@@ -88,9 +88,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func appendToTextField(string: String!, view: UITextView!) {
         // TextFieldにtext追加
-        guard let textdata = string else { return }
-        print(textdata)
-        view.text = view.text.appending("\n\(textdata)")
+        guard let string = string else { return }
+        print(string)
+        view.text = view.text.appending("\n\(string)")
     }
 }
-
